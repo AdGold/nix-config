6081bee16477b2cf4fca54876d34a9ed81ed2473
Adrian Goldwaser <ad.goldwaser@gmail.com>
2025-04-17 21:00:02 +0100

    Improve ranger setup

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -10,14 +10,15 @@ in
     ".bash_profile".source = mkOutOfStoreLink "bash_profile";
     ".gitconfig".source = mkOutOfStoreLink "gitconfig";
     ".config/nvim".source = mkOutOfStoreLink "config/nvim";
-    ".config/kitty/kitty.conf".source = mkOutOfStoreLink "config/kitty/kitty.conf";
-    ".config/ranger/rc.conf".source = mkOutOfStoreLink "config/ranger/rc.conf";
     ".tmux.conf".source = mkOutOfStoreLink "tmux.conf";
     ".p10k.zsh".source = mkOutOfStoreLink "p10k.zsh";
     ".gitignore".source = mkOutOfStoreLink "gitignore";
     ".pythonstartup.py".source = mkOutOfStoreLink "pythonstartup.py";
+    ".config/ranger/rc.conf".source = mkOutOfStoreLink "config/ranger/rc.conf";
+    ".config/ranger/scope.sh".source = mkOutOfStoreLink "config/ranger/scope.sh";
     ".config/ranger/colorschemes/dracula.py".source = mkOutOfStoreLink "config/ranger/colorschemes/dracula.py";
     ".config/zathura/zathurarc".source = mkOutOfStoreLink "config/zathura/zathurarc";
+    ".config/kitty/kitty.conf".source = mkOutOfStoreLink "config/kitty/kitty.conf";
     ".config/kitty/dracula.conf".source = mkOutOfStoreLink "config/kitty/dracula.conf";
     ".ssh/config".source = mkOutOfStoreLink "ssh/config";
     ".scripts".source = mkOutOfStoreLink "scripts";
